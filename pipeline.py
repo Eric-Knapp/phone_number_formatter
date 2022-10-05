71a83e35cc95e286f563ecfd6320069e09f94fd4
Eric Knapp <eric.knapp011@gmail.com>
2022-10-05 13:57:14 -0400

    simple pipeline- address format

---
+++ pipeline.py
@@ -0,0 +1,16 @@
+from codecs import raw_unicode_escape_decode
+from re import S
+import sys
+
+for line in sys.stdin:
+    raw_phone_num = line.strip()
+    
+    area_code = raw_phone_num[0:3]
+    first_3 = raw_phone_num[3:6]
+    last_4 = raw_phone_num[6:]
+    
+    print('(%s) %s-%s' % (area_code, first_3, last_4))
+    
+    
+    # run txt file with phone numbers not formatted:
+    # cat input.txt | python pipline.py > output.txt
